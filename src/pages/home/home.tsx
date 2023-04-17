161eea9e187bc1dc39edfd2ef68c8143a09ce3d5
Letícia Calixto <81197715+LeticiaCalixto@users.noreply.github.com>
2023-04-17 15:47:02 -0300

    update

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,28 +1,44 @@
 import "./style/style.css";
 import Logo from "../../assets/logo.png";
+import Config from "../../assets/icon-config.png";
 import Table from "./components/table";
-import { Button, Col, Row, Card } from "react-bootstrap";
+import { Col, Row, Card } from "react-bootstrap";
+import { useState } from "react";
 // import RadialChart from "./components/radialChart";
 
 export default function Login() {
 
+  const [openModal, setOpenModal] = useState<boolean>(false)
+  const handleModal = (e: boolean) => setOpenModal(e)
+
+  const handleOpenModal = async () => {
+    console.log('openModal', openModal)
+    handleModal(true)
+  }
+
   return (
-      <div className="container">
-        <Card.Body>
-          <Row>
-            <img className="icon" src={Logo} />
-            <h4 className="title">
-              <span>Tempmeter</span>
-            </h4>
-          </Row>
+    <div className="container-fluid" style={{ backgroundColor: "#392F5A" }}>
+      <Card.Body>
+      <Row>
+          <img className="icon" src={Logo} />
+          <h4 className="title">
+            <span>Tempmeter</span>
+          </h4>
+          <div className="button-config">
+            <button type="button" className="btn" style={{backgroundColor: "transparent", width: 70}} data-toggle="modal" data-target="#ModalConfig">
+              <img className="icon-config" src={Config} />
+            </button>
+          </div>
+        </Row>
 
-          <Row>
-            <Col>  
-                <Table />
-            </Col>
-          </Row>
+        <Row>
+          <Col>
+            <Table />
+          </Col>
+        </Row>
+      </Card.Body>
+        
 
-        </Card.Body>
-      </div>
+    </div>
   );
 }
